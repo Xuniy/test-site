020c882154710e03669841617bcfb2dc506ab855
Xuniy <xuniy.twitch@gmail.com>
2026-04-06 15:03:20 +0200

    8

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import ContactPage from './pages/Contact';
 export default function App() {
   return (
     <LanguageProvider>
-      <BrowserRouter>
+      <BrowserRouter basename="/test-site">
         <Routes>
           <Route element={<Layout />}>
             <Route path="/" element={<Home />} />
